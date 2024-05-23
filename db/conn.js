9503a657608bb09231742ea35f3c45b624e88a5b
TiagoEngSoft <tiagoengsoftdev@gmail.com>
2024-05-22 21:00:32 -0300

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -4,12 +4,7 @@ const mongoose = require('mongoose');
 async function main() {
   const mongoURI = process.env.MONGODB_URI;
   try {
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    });
+    await mongoose.connect(mongoURI);
     console.log('Conectou com Mongoose!');
   } catch (error) {
     console.error('Erro ao conectar com Mongoose:', error);

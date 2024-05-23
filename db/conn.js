3a97badf8716cdca818011ea4d16869d0e76e31f
TiagoEngSoft <tiagoengsoftdev@gmail.com>
2024-05-22 21:20:10 -0300

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+// Somente para ambiente local
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const mongoose = require('mongoose');
 
 async function main() {
